49ac76a0d385283e24348279a7e6d3581aed24ef
Sk Mahammad Afzal <138036484+SkAfzal-04@users.noreply.github.com>
2025-07-10 14:20:27 +0530

    Update Scan.js

--- admin/src/pages/Scan.js
+++ admin/src/pages/Scan.js
@@ -30,7 +30,7 @@ const BarcodeScanner = () => {
 
         // Setup WebSocket
         //socketRef.current = new WebSocket("ws://localhost:4000");
-        socketRef.current = new WebSocket("wss://https://scanify-dun.vercel.app");
+        socketRef.current = new WebSocket("wss://scanify-dun.vercel.app");
 
         socketRef.current.onopen = () => {
           socketRef.current.send(JSON.stringify({
